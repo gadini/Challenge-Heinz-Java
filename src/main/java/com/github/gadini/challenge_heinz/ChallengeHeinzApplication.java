b35b96065b622c8b49af3d6e9b07cf747e6fcea9
gustur0 <alvesrochagadini@gmail.com>
2024-06-17 12:45:23 -0300

    Initial

---
+++ src/main/java/com/github/gadini/challenge_heinz/ChallengeHeinzApplication.java
@@ -0,0 +1,13 @@
+package com.github.gadini.challenge_heinz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeHeinzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeHeinzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/gadini/challenge_heinz/ChallengeHeinzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gadini.challenge_heinz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeHeinzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
